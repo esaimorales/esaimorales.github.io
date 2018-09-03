5cf624792a6e4c34d34541ab870019a6f8ddf264
Esai Morales <esaimn96@gmail.com>
2018-09-02 21:05:13 -0700

    nav

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-// $(".menu-toggle").on('click', function() {
-//   $(this).toggleClass("on");
-//   $('.menu-section').toggleClass("on");
-//   $("nav ul").toggleClass('hidden');
-// });
-
-
-function toggleNavigation(){
-    console.log("Navigation Clicked!");
-    nav = document.getElementById('sideNav');
-
-    if (nav.style.display == "none"){
-        nav.style.display = "block";
-    }
-    else {
-        nav.style.display = "none";
-    }
-
-    // document.getElementById('sideNav').style.display = "block";
-    // document.getElementById('contact').style.display = "none";
-    // document.getElementById('sideNav').style.height = "40%";
-}
+/* Open Menu */
+  $( ".open-close-btn" ).on('click touchstart', function(e) {
+      // prevent default anchor click
+      e.preventDefault();
+      $(".overlay").toggleClass("overlay-open");
+      $("#hamburger-icon").toggleClass("hamburger-open");
+  });
